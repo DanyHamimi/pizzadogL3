0b184deea16491927815ca03196fd2d0cdf1d004
Dany <dany.hamimi@gmail.com>
2022-05-23 16:42:51 +0200

    commit

--- server.js
+++ server.js
@@ -6,58 +6,109 @@ const port = process.env.PORT || 5000;
 const path = require('path');
 const bcrypt = require('bcrypt');
 
-var mysql = require('mysql');
 
 var session = require('express-session')({
     secret: "my-secret",
     resave: true,
     saveUninitialized: true
   });
-//var sharedsession = require("express-socket.io-session");
-//const { emit } = require('process');
-//const { Socket } = require('engine.io');
-
 app.use(session);
-
-//io.use(sharedsession(session));
-
-var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'pizzadog',
-    charset : 'utf8mb4',
-    dateStrings: true,
+//postgresql connection
+const { Client } = require('pg');
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "myPassword",
+    database: "pizzadog",
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log(`${__dirname}/public/index.html`);
-    console.log("Connected!");
+client.connect();
+
+let pizzaName;
+
+let toppingPizz;
+
+let starterName;
+
+let boissonName;
+
+client.query('Select * from Pizza', (err, res) => {
+    if(err) {
+        console.log(err.stack);
+    }else{
+        pizzaName = res.rows;
+    }
+    //client.end(); // Ne pas end sinon plus possible de faire aucune demande
+})
+
+client.query('Select * from topping', (err, res) => {
+    if(err) {
+        console.log(err.stack);
+    }else{
+        toppingPizz = res.rows;
+    }
+    //client.end(); // Ne pas end sinon plus possible de faire aucune demande
+})
+
+
+client.query('Select * from boisson', (err, res) => {
+    if(err) {
+        console.log(err.stack);
+    }else{
+        boissonName = res.rows;
+    }
+    //client.end(); // Ne pas end sinon plus possible de faire aucune demande
+})
+
+client.query('Select * from starter', (err, res) => {
+    if(err) {
+        console.log(err.stack);
+    }else{
+        starterName = res.rows;
+    }
+    //client.end(); // Ne pas end sinon plus possible de faire aucune demande
+})
+
+
+//ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join('./views'));
+
+
+
+
+var session = require('express-session')({
+    secret: "my-secret",
+    resave: true,
+    saveUninitialized: true
   });
 
+app.use(session);
 app.use(express.static('img'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
 app.use(express.static(__dirname + '/public'));
 //app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/', (req,res) => {
-    res.sendFile(`${__dirname}\public\index.html`)
-    /*if(request.session.loggedin != 1){
-        request.session.loggedin = 0;
-    }*/
+    console.log(toppingPizz);
+    res.render("index.ejs", {message: "rayane teste", pizzaName: pizzaName, toppingPizz: toppingPizz, starterName: starterName, boissonName: boissonName});
 })
 
-app.post('/index.html', function (req, res) {
-    res.redirect(`/index.html?name=${req.body.name}`)
-    console.log(`Full name is:${req.body.name} .`);
+app.get('/login', (req,res) => {
+    res.render("login.ejs", {tried : "0"});
+})
+
+
+app.get('/register', (req,res) => {
+    res.render("register.ejs", {tried : "0"});
+})
+
+app.get('/basket', (req,res) => {
+    res.render("basket.ejs", {});
 })
 
 app.post('/register', function(request, response) {
@@ -66,67 +117,63 @@ app.post('/register', function(request, response) {
     var email = request.body.email;
     var password = request.body.password;
 
-
     if(name && surename && email && password){
-        connection.query('SELECT * FROM users WHERE mail = ?', [email], function(error, results, fields) {
-            console.log(results.length);
-            if(results.length > 0){
-                response.redirect('/registerB.html')
-            } else {
-                bcrypt.hash(password, 10, function(err, hash) {
-                    connection.query('INSERT INTO users (name, surename, mail, password) VALUES (?, ?, ?, ?)', [name, surename, email, hash], function(error, results, fields) {
-                        if(error) {
-                            console.log(error);
-                        } else {
-                            response.redirect('/login.html');
-                        }
+        client.connect;
+        client.query("SELECT * FROM users WHERE mail = '"+email+"'", (err, res) => {
+            if(err) {
+                console.log(err.stack);
+            }else{
+                console.log(res.rows.length);
+                if(res.rows.length > 0){
+                    response.render("register.ejs", {tried : "1"});
+                } else {
+                    bcrypt.hash(password, 10, function(err, hash) {
+                        let token = Array(100).fill().map(()=>"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".charAt(Math.random()*62)).join(""); // TODO : Vérifier que le token est unique
+                        client.query("INSERT INTO users VALUES (1,'"+name+"','"+surename+"','"+email+"',NULL,NULL,'"+token+"','"+hash+"')", (err, res) => { // TODO : Changer uniqueID pr autoincrement
+                            if(err) {
+                                console.log(err.stack);
+                            } else {
+                                response.redirect('/login');
+                            }
+                        });
                     });
-                });
-            }
+                }
+            } 
+
         });
-    };
-});
+    }
+})
 
 app.post('/login', function(request, response) {
     var mail = request.body.mail;
     var password = request.body.password;
     if(mail && password){
-        connection.query('SELECT * FROM users WHERE mail = ?', [mail], function(error, results, fields) {
-            if(results.length > 0){
-                bcrypt.compare(password, results[0].password, function(err, res) {
-                    if(res == true){
-                        request.session.loggedin = 1;
-                        request.session.name = results[0].name;
-                        request.session.surename = results[0].surename;
-                        request.session.mail = results[0].mail;
-                        request.session.id = results[0].id;
-                        response.redirect('/index.html');
-                        console.log('mail: ' + request.session.mail+' is logged in');
-                    } else {
-                        response.redirect('/login.html');
-                    }
-                });
-            } else {
-                response.redirect('/register.html');
+        client.query("SELECT * FROM users WHERE mail = '"+mail+"'", (err, res) => {
+            if(err) {
+                console.log(err.stack);
+            }else{
+                if(res.rows.length > 0){
+                    //console.log(res.rows[0].password);
+                    let token = res.rows[0].token;
+
+                    bcrypt.compare(password, res.rows[0].password, function(err, res) {
+                        if(res == true){
+                            request.session.loggedin = 1;
+                            response.cookie("token", token);
+                            request.session.token = token;
+                            response.redirect('/');
+                            console.log('token: ' + request.session.token+' is logged in');
+                        } else {
+                            response.render("login.ejs", {tried : "1"});
+                        }
+                    });
+                } else {
+                    response.redirect('/register');
+                }
             }
         });
     }
 });
-
-/*io.on('connection', (socket) =>{
-    var session = socket.handshake.session;
-
-    socket.on('checkmail', (mail) => {
-        console.log("mail");
-        connection.query('SELECT * FROM users WHERE mail = ?', [mail], function(error, results, fields) {
-            if(results.length > 0){
-                io.emit('checkmail', 'User already exists');
-            } else {
-                io.emit('checkmail', 'User does not exist');
-            }
-        }); 
-    });
-});*/
 
 server.listen(port, () =>{
     console.log(`Ca demarre sur le port ${port}`);
