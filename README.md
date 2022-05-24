f06ae5aab8aea39e35f7f3ba9ecd68844959c6af
Dany <dany.hamimi@gmail.com>
2022-05-24 10:07:19 +0200

    commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # PizzaDog
+HAMIMI DANY / 21952735
+KAABECHE Rayane / 
+L3 INFORMATIQUE GROUPE 3
+Projet de Programmation WEB : 
+
+Afin de pouvoir utiliser le Site vous devez au préalable installer la base de données présente dans le fichier
+PizzaDogData.sql via les commandes suivantes dans la base de donnée du nom "pizzadog" que vous aurez créee
+
+CREATE DATABASE pizzadog;
+\q
+psql pizzadog < PizzaDogData.sql
+
+Votre base de donnée postgresql devra avoir les identifiants suivants ou bien vous pourrez les changer depuis le
+fichier server.js :
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "myPassword",
+    database: "pizzadog",
+
+Ensuite vous pourrez lancer le site via les commandes suivantes :
+
+npm i
+npm install express
+npm install bcrypt
+npm install cookie-parser
+npm install express-session
+npm install pg
+npm install ejs
+npm start
+vous pourrez accéder au site via localhost:5000
+
+Les fonctionalités vous sont présentées dans la vidéo suivante :
+
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
-const server = require('http') .createServer(app);
+const server = require('http').createServer(app);
 //const io = require('socket.io')(server, {'pingTimeout': 180000, 'pingInterval': 25000});
 const port = process.env.PORT || 5000;
 const path = require('path');
 const bcrypt = require('bcrypt');
+//cookie parser
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 
 var session = require('express-session')({
     secret: "my-secret",
     resave: true,
     saveUninitialized: true
-  });
+});
 app.use(session);
 //postgresql connection
 const { Client } = require('pg');
@@ -35,19 +38,23 @@ let boissonName;
 
 let menuName;
 
+let commande;
+
+let fin;
+
 client.query('Select * from Pizza', (err, res) => {
-    if(err) {
+    if (err) {
         console.log(err.stack);
-    }else{
+    } else {
         pizzaName = res.rows;
     }
     //client.end(); // Ne pas end sinon plus possible de faire aucune demande
 })
 
 client.query('Select * from topping', (err, res) => {
-    if(err) {
+    if (err) {
         console.log(err.stack);
-    }else{
+    } else {
         toppingPizz = res.rows;
     }
     //client.end(); // Ne pas end sinon plus possible de faire aucune demande
@@ -55,18 +62,18 @@ client.query('Select * from topping', (err, res) => {
 
 
 client.query('Select * from boisson', (err, res) => {
-    if(err) {
+    if (err) {
         console.log(err.stack);
-    }else{
+    } else {
         boissonName = res.rows;
     }
     //client.end(); // Ne pas end sinon plus possible de faire aucune demande
 })
 
 client.query('Select * from starter', (err, res) => {
-    if(err) {
+    if (err) {
         console.log(err.stack);
-    }else{
+    } else {
         starterName = res.rows;
     }
     //client.end(); // Ne pas end sinon plus possible de faire aucune demande
@@ -84,55 +91,275 @@ var session = require('express-session')({
     secret: "my-secret",
     resave: true,
     saveUninitialized: true
-  });
+});
 
 app.use(session);
 app.use(express.static('img'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
 //app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     console.log(toppingPizz);
-    res.render("index.ejs", {message: "rayane teste", pizzaName: pizzaName, toppingPizz: toppingPizz, starterName: starterName, boissonName: boissonName});
+    let prom = new Promise((resolve, reject) => {
+        client.query('Select * from menu', (err, res) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                menuName = res.rows;
+            }
+            resolve(menuName);
+        })
+    })
+    prom.then(() => {
+        console.log(menuName);
+        res.render("index.ejs", { message: "rayane teste", pizzaName: pizzaName, toppingPizz: toppingPizz, starterName: starterName, boissonName: boissonName, menuName: menuName });
+    })
+
 })
 
-app.get('/login', (req,res) => {
-    res.render("login.ejs", {tried : "0"});
+app.get('/login', (req, res) => {
+    res.render("login.ejs", { tried: "0" });
 })
 
 
-app.get('/register', (req,res) => {
-    res.render("register.ejs", {tried : "0"});
+app.get('/register', (req, res) => {
+    res.render("register.ejs", { tried: "0" });
 })
 
-app.get('/basket', (req,res) => {
-    res.render("basket.ejs", {});
+let command_info;
+
+app.get('/etat/:id', (req, res) => {
+    var sqlLine = "Select * from commande where commande_id = " + req.params.id;
+    let prom = new Promise((resolve, reject) => {
+        client.query(sqlLine, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                console.log(sqlLine);
+                fin=res.rows;
+                resolve(res);
+            }
+        })    
+    })
+    prom.then(() => {
+            console.log('coucou');
+  })
+  var sqlLine2 = "Select * from commande_ligne where commande_id = " + req.params.id;
+  let prom2 = new Promise((resolve, reject) => {
+    client.query(sqlLine2, (err, res) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            console.log(sqlLine2);
+            command_info=res.rows;
+            resolve(res);
+        }
+    })    
+    })
+    prom2.then(() => {
+        console.log('coucou');
+        res.send('<script>window.location.href="/etat";</script>');
+    })
+    //res.render("etat.ejs");
 })
 
-app.post('/register', function(request, response) {
+app.get('/etat', (req, res) => {
+    let pizza ;
+    let boisson;
+    res.render("etat.ejs" , { fin: fin });
+    
+})
+
+app.get('/basket', (req, res) => {
+    //console.log('Cookies: ', req.cookies.basket);
+    const elmns = [];
+    elmns.push(req.cookies.basket);
+    //console.log(elmns.join(''));
+    //regex '/' elmns
+    var bisElmns = elmns.join('').split('/');
+    bisElmns = elmns.join('').split(',');
+    res.render("basket.ejs", { basket: bisElmns });
+})
+
+function getCookiePers(req){
+    const elmns = [];
+    elmns.push(req.cookies.basket);
+    //console.log(elmns.join(''));
+    //regex '/' elmns
+    var bisElmns = elmns.join('').split('/');
+    return bisElmns;
+}
+
+app.get('/order', (req, res) => {
+    //console.log('Cookies: ', req.cookies.basket);
+    const elmns = [];
+    elmns.push(req.cookies.basket);
+    //console.log(elmns.join(''));
+    //regex '/' elmns
+    var bisElmns = elmns.join('').split('/');
+    bisElmns = elmns.join('').split(',');
+    res.render("order.ejs", { basket: bisElmns });
+})
+
+function livreur(res){
+    let prom = new Promise((resolve, reject) => {
+        client.query('Select * from commande where status !=2', (err, res) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                commande = res.rows;
+            }
+            resolve(commande);
+        })
+    })
+    prom.then(() => {
+        //console.log(commande);
+        res.render("livreur.ejs", { commande: commande });
+    })
+}
+
+let test_pers;
+
+function get_id_commande(res){
+    let retur;
+    let prom = new Promise((resolve, reject) => {
+        client.query('Select MAX(commande_id) from commande', (err, res) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                retur = res.rows;
+            }
+            resolve(retur);
+        })
+    })
+    prom.then(() => {
+        console.log(retur);
+        test_pers = retur;
+    })
+}
+
+app.get('/livreur', (req, res) => {
+    livreur(res);
+})
+app.get('/livreur/:id', (req, res) => {
+    var id_command = 0;
+    var sqlLine = "UPDATE commande SET status = 1 WHERE commande_id = " + req.params.id;
+    let prom = new Promise((resolve, reject) => {
+        client.query(sqlLine, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                console.log(sqlLine);
+                resolve(res);
+            }
+        })    
+    })
+    prom.then(() => {
+            console.log('coucou');
+            //res.render("/livreur");
+            res.send('<script>window.location.href="/livreur";</script>');
+        
+  })
+})
+
+app.get('/livre/:id', (req, res) => {
+    var id_command = 0;
+    var sqlLine = "UPDATE commande SET status = 2 WHERE commande_id = " + req.params.id;
+    let prom = new Promise((resolve, reject) => {
+        client.query(sqlLine, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                console.log(sqlLine);
+                resolve(res);
+            }
+        })    
+    })
+    prom.then(() => {
+            console.log('coucou');
+            //res.render("/livreur");
+            res.send('<script>window.location.href="/livreur";</script>');
+        
+  })
+})
+
+app.post('/orderConfirm', function (request, response){
+    let prom2 = new Promise((resolve, reject) => {
+        client.query('Select MAX(commande_id) from commande', (err, resi) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                console.log(resi.rows[0].max);
+        var sqlLine = "INSERT INTO commande (name_client, adresse, status, prix) VALUES ('" + request.body.Nom + "','" + request.body.Adresse +"','"+0+"', '"+ 10 +"')";
+        let danydan = getCookiePers(request);
+        let prom = new Promise((resolve, reject) => {
+            client.query(sqlLine, (err, res) => {
+                if (err) {
+                    console.log(err.stack);
+                } else {
+                    console.log(sqlLine);
+                    resolve(res);
+                }
+            })    
+        })
+        prom.then(() => {
+            console.log('coucou');
+            for (var i=0; i < danydan.length-1; i=i+8) {
+                var newsqlLine = "INSERT INTO commande_ligne values ('" + danydan[i+1] + "','" + danydan[i+7] + "','" + (resi.rows[0].max+1)  + "','" + danydan[i+3] + "','" + danydan[i+4] + "','" + danydan[i+5] + "')";
+                let prom3 = new Promise((resolve, reject) => {
+                    client.query(newsqlLine, (err, res) => {
+                        if (err) {
+                            console.log(err.stack);
+                        } else {
+                            console.log(newsqlLine);
+                            resolve(res);
+                        }
+                    })    
+                })
+            prom3.then(() => {
+                    console.log('coucou');
+            })
+        }
+            response.send('<script>window.location.href="/etat/'+(resi.rows[0].max+1)+'";</script>');
+        })
+                
+                resolve(resi.rows[0].max);
+
+            }
+            
+        })
+    })
+    prom2.then((val) => {
+        console.log("ici");
+    })
+    //get_id_commande(response);
+    
+});
+
+app.post('/register', function (request, response) {
     var name = request.body.name;
     var surename = request.body.surename;
     var email = request.body.email;
     var password = request.body.password;
 
-    if(name && surename && email && password){
+    if (name && surename && email && password) {
         client.connect;
-        client.query("SELECT * FROM users WHERE mail = '"+email+"'", (err, res) => {
-            if(err) {
+        client.query("SELECT * FROM users WHERE mail = '" + email + "'", (err, res) => {
+            if (err) {
                 console.log(err.stack);
-            }else{
+            } else {
                 console.log(res.rows.length);
-                if(res.rows.length > 0){
-                    response.render("register.ejs", {tried : "1"});
+                if (res.rows.length > 0) {
+                    response.render("register.ejs", { tried: "1" });
                 } else {
-                    bcrypt.hash(password, 10, function(err, hash) {
-                        let token = Array(100).fill().map(()=>"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".charAt(Math.random()*62)).join(""); // TODO : Vérifier que le token est unique
-                        client.query("INSERT INTO users VALUES (1,'"+name+"','"+surename+"','"+email+"',NULL,NULL,'"+token+"','"+hash+"')", (err, res) => { // TODO : Changer uniqueID pr autoincrement
-                            if(err) {
+                    bcrypt.hash(password, 10, function (err, hash) {
+                        let token = Array(100).fill().map(() => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".charAt(Math.random() * 62)).join(""); // TODO : Vérifier que le token est unique
+                        client.query("INSERT INTO users VALUES (1,'" + name + "','" + surename + "','" + email + "',NULL,NULL,'" + token + "','" + hash + "')", (err, res) => { // TODO : Changer uniqueID pr autoincrement
+                            if (err) {
                                 console.log(err.stack);
                             } else {
                                 response.redirect('/login');
@@ -140,33 +367,33 @@ app.post('/register', function(request, response) {
                         });
                     });
                 }
-            } 
+            }
 
         });
     }
 })
 
-app.post('/login', function(request, response) {
+app.post('/login', function (request, response) {
     var mail = request.body.mail;
     var password = request.body.password;
-    if(mail && password){
-        client.query("SELECT * FROM users WHERE mail = '"+mail+"'", (err, res) => {
-            if(err) {
+    if (mail && password) {
+        client.query("SELECT * FROM users WHERE mail = '" + mail + "'", (err, res) => {
+            if (err) {
                 console.log(err.stack);
-            }else{
-                if(res.rows.length > 0){
+            } else {
+                if (res.rows.length > 0) {
                     //console.log(res.rows[0].password);
                     let token = res.rows[0].token;
 
-                    bcrypt.compare(password, res.rows[0].password, function(err, res) {
-                        if(res == true){
+                    bcrypt.compare(password, res.rows[0].password, function (err, res) {
+                        if (res == true) {
                             request.session.loggedin = 1;
                             response.cookie("token", token);
                             request.session.token = token;
                             response.redirect('/');
-                            console.log('token: ' + request.session.token+' is logged in');
+                            console.log('token: ' + request.session.token + ' is logged in');
                         } else {
-                            response.render("login.ejs", {tried : "1"});
+                            response.render("login.ejs", { tried: "1" });
                         }
                     });
                 } else {
@@ -177,6 +404,6 @@ app.post('/login', function(request, response) {
     }
 });
 
-server.listen(port, () =>{
+server.listen(port, () => {
     console.log(`Ca demarre sur le port ${port}`);
 });
